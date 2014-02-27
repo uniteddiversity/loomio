2e37372203ac59da21d646d50a1fa054dcf8c5a9
Hannah Salmon <hr.salmon@gmail.com>
2014-02-27 17:20:21 +1300

    Fix migration

--- db/migrate/20140206023957_make_invitations_polymorphic.rb
+++ db/migrate/20140206023957_make_invitations_polymorphic.rb
@@ -1,18 +1,22 @@
+require 'ruby-progressbar'
 class MakeInvitationsPolymorphic < ActiveRecord::Migration
   def up
     change_table :invitations do |t|
       t.references :invitable, polymorphic: true
     end
 
+    invitation_count = Invitation.count
+    progress_bar = ProgressBar.create( format: "(\e[32m%c/%C\e[0m) %a |%B| \e[31m%e\e[0m ", progress_mark: "\e[32m/\e[0m", total: invitation_count )
+
     Invitation.reset_column_information
     ActiveRecord::Base.record_timestamps = false
     begin
-      puts "Updating #{Invitation.last.id} Invitation records to polymorphic"
+      puts "Updating #{invitation_count} Invitation records to polymorphic"
 
       Invitation.update_all(invitable_type: 'Group')
 
       Invitation.find_each do |i|
-        p i.id if i.id % 100 == 0
+        progress_bar.increment
 
         i.update_attribute(:invitable_id, i.group_id)
       end
